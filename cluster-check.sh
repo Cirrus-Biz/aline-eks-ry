14fe0cf9b0acb055ea613d76232c95e94b859a4c
noodleween <happyguy122x@gmail.com>
2022-09-27 15:53:00 -0600

    CB-91 Refactor: Cluster Check script

--- cluster-check.sh
+++ cluster-check.sh
@@ -2,8 +2,8 @@ aws eks describe-cluster --name my-eks-ry 1>/dev/null 2>&1
 
 status_code=$?
 
-if [ $status_code == 0 ]; then 
-    echo "code is zero"
-else
+if [ $status_code -ne 0 ]; then 
     eksctl create cluster --name my-eks-ry --region us-east-1 --zones us-east-1a,us-east-1b --fargate
+else
+    echo "code is zero"
 fi
