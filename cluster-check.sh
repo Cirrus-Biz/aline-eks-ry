860a27af5b467a7f3622e1bec771b7a4e3f7e12d
noodleween <happyguy122x@gmail.com>
2022-09-27 16:12:39 -0600

    CB-91 Refactor: Cluster Check script

--- cluster-check.sh
+++ cluster-check.sh
@@ -1,6 +1,10 @@
+set +e
+
 aws eks describe-cluster --name my-eks-ry 1>/dev/null 2>&1
 
 status_code=$?
+
+set -e
 
 if [ $status_code -ne 0 ]; then 
     eksctl create cluster --name my-eks-ry --region us-east-1 --zones us-east-1a,us-east-1b --fargate
