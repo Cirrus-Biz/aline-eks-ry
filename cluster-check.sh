adf3f4e30fe7eb9945f41f72720a7f83d5ef7ca5
noodleween <happyguy122x@gmail.com>
2022-09-27 16:27:49 -0600

    CB-91 Refactor: Cluster Check script

--- cluster-check.sh
+++ cluster-check.sh
@@ -7,7 +7,7 @@ status_code=$?
 set -e
 
 if [ $status_code -ne 0 ]; then 
-    eksctl create cluster --name my-eks-ry --region us-east-1 --zones us-east-1a,us-east-1b --fargate
+    eksctl create cluster --name my-eks-ry --region us-east-1 --zones us-east-1a,us-east-1b --fargate --profile ry-eks
 else
     echo "code is zero"
 fi
