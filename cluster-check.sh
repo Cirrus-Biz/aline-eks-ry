4f2e276e8213b05a0ee940327935ed0cbf3a6a9e
noodleween <happyguy122x@gmail.com>
2022-09-27 17:00:07 -0600

    CB-91 Add: helm stages and apply stage

--- cluster-check.sh
+++ cluster-check.sh
@@ -8,6 +8,20 @@ set -e
 
 if [ $status_code -ne 0 ]; then 
     eksctl create cluster --name my-eks-ry --region us-east-1 --zones us-east-1a,us-east-1b --fargate --profile ry-eks
+
+    eksctl utils associate-iam-oidc-provider --region=us-east-1 --cluster=my-eks-ry --approve
+
+    eksctl create iamserviceaccount \
+    --cluster=my-eks-ry \
+    --namespace=kube-system \
+    --name=aws-load-balancer-controller \
+    --role-name "AmazonEKSLoadBalancerControllerRole" \
+    --attach-policy-arn=arn:aws:iam::744758641322:policy/AWSLoadBalancerControllerIAMPolicy \
+    --approve
+
+    echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
+    echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
+    .  ~/.bash_profile
 else
     echo "code is zero"
 fi
